6566255788b79159e548d075cc9d48e0d5825297
beastthor <beastthor.ykc@gmail.com>
2025-12-05 10:33:22 +0530

    adding allchanges

--- src/main/java/com/pluralsight/beanconfig/util/PieCalculator.java
+++ src/main/java/com/pluralsight/beanconfig/util/PieCalculator.java
@@ -2,7 +2,6 @@ package com.pluralsight.beanconfig.util;
 
 import org.springframework.stereotype.Component;
 
-@Component
 public class PieCalculator {
     public int calculateSlices(int diameter) {
         return (int) Math.floor((Math.PI * diameter * diameter) / 14.0);
